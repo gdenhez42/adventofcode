6a23c21c3b459d711119fe36cbda3a788eab322c
gdenhez <gdenhez42@gmail.com>
2022-12-06 19:12:01 -0500

    day6

--- 2022/README.md
+++ 2022/README.md
@@ -7,7 +7,7 @@ My colleagues and I challenged ourselves to learn new languages and the Advent o
 3. [Day 3](https://adventofcode.com/2022/day/3): Python
 4. [Day 4](https://adventofcode.com/2022/day/4): C++
 5. [Day 5](https://adventofcode.com/2022/day/5): VB.NET
-6. C#
+6. [Day 6](https://adventofcode.com/2022/day/6): C#
 7. PHP
 8. JavaScript
 9. Bash
---
+++ 2022/day6/Program.cs
@@ -0,0 +1,35 @@
+﻿string text = System.IO.File.ReadAllText("input.txt");
+
+// Part 1
+for(int i=0; i<text.Length-3; i++) {
+    // too lazy to write a loop and not lazy enough to copy paste
+    if (text[i] != text[i+1] &&
+        text[i] != text[i+2] &&
+        text[i] != text[i+3] &&
+        text[i+1] != text[i+2] &&
+        text[i+1] != text[i+3] &&
+        text[i+2] != text[i+3])
+    {
+        Console.WriteLine(i + 4);
+        break;
+    }
+}
+
+// Part2
+for(int i=0; i<text.Length-13; i++) {
+    // ok you win... I'll write some loops
+    bool allDifferent = true;
+    for (int j = 0; j < 14; j++) {
+        for (int k = j+1; k < 14; k++) {
+            if (text[i+j] == text[i+k]) {
+                allDifferent = false;
+            }
+        }
+    }
+    if (allDifferent)
+    {
+        Console.WriteLine(i + 14);
+        break;
+    }
+}
+
