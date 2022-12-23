e24f7347d0f65ecbcf5a1987f4c25b4d9188d7e4
gdenhez <gdenhez42@gmail.com>
2022-12-22 20:17:17 -0500

    figuring clojure

--- 2022/day18/day18.clj
+++ 2022/day18/day18.clj
@@ -8,5 +8,18 @@
    (clojure.string/split-lines
    (slurp "/home/gdenhez/git/adventofcode/2022/day18/input_test.txt"))))
 
-(read_input)
+(defn edges [x y z]
+  (let [edge1 ["x" x]
+        edge2 ["x" (+ x 1)]
+        edge3 ["y" y]
+        edge4 ["y" (+ y 1)]
+        edge5 ["z" z]
+        edge6 ["z" (+ z 1)]]
+    (set (list edge1 edge2 edge3 edge4 edge5 edge6))))
+
+
+(defn count_edges [elem arr]
+  (reduce conj arr #{}))
+
+(apply edges '(1 2 2))
 
