ae809a3baa1ef791c06f6b2d1907cfd85d00c553
gdenhez <gdenhez42@gmail.com>
2022-12-24 21:44:38 -0500

    day18 part 1

--- 2022/day18/day18.clj
+++ 2022/day18/day18.clj
@@ -1,25 +1,73 @@
 (require '[clojure.string])
+(require '[clojure.set])
 
 (defn read_coordinate [e]
-  (map (fn [x] (Integer/parseInt x)) (clojure.string/split e #",")))
+  (into [] (map (fn [x] (Integer/parseInt x)) (clojure.string/split e #","))))
 
 (defn read_input []
   (map read_coordinate
    (clojure.string/split-lines
-   (slurp "/home/gdenhez/git/adventofcode/2022/day18/input_test.txt"))))
+   (slurp "/home/gdenhez/git/adventofcode/2022/day18/input.txt"))))
 
-(defn edges [x y z]
-  (let [edge1 ["x" x]
-        edge2 ["x" (+ x 1)]
-        edge3 ["y" y]
-        edge4 ["y" (+ y 1)]
-        edge5 ["z" z]
-        edge6 ["z" (+ z 1)]]
-    (set (list edge1 edge2 edge3 edge4 edge5 edge6))))
+(def input (read_input))
 
+(def x_plans (group-by (fn [[x _ _]] x) input))
+(def y_plans (group-by (fn [[_ y _]] y) input))
+(def z_plans (group-by (fn [[_ _ z]] z) input))
 
-(defn count_edges [elem arr]
-  (reduce conj arr #{}))
+(defn is_not_blocked_above [[x y z]]
+  (let [elem (x_plans (- x 1))]
+    (if (= elem nil) true
+        (= (some (fn [[_ ey ez]] (and (= y ey) (= z ez))) elem) nil))))
 
-(apply edges '(1 2 2))
+(defn is_not_blocked_below [[x y z]]
+  (let [elem (x_plans (+ x 1))]
+    (if (= elem nil) true
+        (= (some (fn [[_ ey ez]] (and (= y ey) (= z ez))) elem) nil))))
 
+(defn is_not_blocked_left [[x y z]]
+  (let [elem (y_plans (- y 1))]
+    (if (= elem nil) true
+        (= (some (fn [[ex _ ez]] (and (= x ex) (= z ez))) elem) nil))))
+
+(defn is_not_blocked_right [[x y z]]
+  (let [elem (y_plans (+ y 1))]
+    (if (= elem nil) true
+        (= (some (fn [[ex _ ez]] (and (= x ex) (= z ez))) elem) nil))))
+
+(defn is_not_blocked_front [[x y z]]
+  (let [elem (z_plans (+ z 1))]
+    (if (= elem nil) true
+        (= (some (fn [[ex ey _]] (and (= x ex) (= y ey))) elem) nil))))
+
+(defn is_not_blocked_back [[x y z]]
+  (let [elem (z_plans (- z 1))]
+    (if (= elem nil) true
+        (= (some (fn [[ex ey _]] (and (= x ex) (= y ey))) elem) nil))))
+
+(def nb_tiles_x
+  (reduce (fn [acc [_ plan]]
+            (+ (count (filter is_not_blocked_above plan))
+               (count (filter is_not_blocked_below plan))
+               acc))
+          0
+          x_plans))
+
+(def nb_tiles_y
+  (reduce (fn [acc [_ plan]]
+            (+ (count (filter is_not_blocked_right plan))
+               (count (filter is_not_blocked_left plan))
+               acc))
+          0
+          y_plans))
+
+(def nb_tiles_z
+  (reduce (fn [acc [_ plan]]
+            (+ (count (filter is_not_blocked_front plan))
+               (count (filter is_not_blocked_back plan))
+               acc))
+          0
+          z_plans))
+
+;; part 1
+(+ nb_tiles_x nb_tiles_y nb_tiles_z)
