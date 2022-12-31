825ed9f8d82ab87ec1668ce0b3b69bde6f1f10a1
gdenhez <gdenhez42@gmail.com>
2022-12-31 09:25:34 -0500

    day21 part2

--- 2022/day21/src/main/scala/Main.scala
+++ 2022/day21/src/main/scala/Main.scala
@@ -31,6 +31,20 @@ def doSomeMath(v1: Long, v2: Long, op: String) : Long =
     case "*" => v1 * v2
     case "/" => v1 / v2
 
+def solveLeft(v2: Long, result: Long, op: String) : Long =
+  op match
+    case "+" => result - v2
+    case "-" => result + v2
+    case "*" => result / v2
+    case "/" => result * v2
+
+def solveRight(v1: Long, result: Long, op: String) : Long =
+  op match
+    case "+" => result - v1
+    case "-" => v1 - result
+    case "*" => result / v1
+    case "/" => v1 / result
+
 
 def part1 (monkeys : List[Monkey]) : Long =
   val vals = scala.collection.mutable.Map[String, Long]()
@@ -56,12 +70,14 @@ def part1 (monkeys : List[Monkey]) : Long =
 
 def part2 (monkeys : List[Monkey]) : Long =
   val vals = scala.collection.mutable.Map[String, Long]()
+  val mathMonkeys = scala.collection.mutable.Map[String, MathMonkey]()
   for (monkey <- monkeys) {
     if (monkey.t == "yell" && monkey.name != "humn") vals(monkey.name) = monkey.asInstanceOf[YellMonkey].value
+    if (monkey.t == "math") mathMonkeys(monkey.name) = monkey.asInstanceOf[MathMonkey]
   }
-  var found = true
-  while (found) {
-    found = false
+  var foundVals = true
+  while (foundVals) {
+    foundVals = false
 
     for (monkey <- monkeys) {
       monkey match
@@ -69,15 +85,55 @@ def part2 (monkeys : List[Monkey]) : Long =
           (vals.get(math.name), vals.get(math.leftMonkey), vals.get(math.rightMonkey)) match
             case (None, Some(v1), Some(v2)) =>
               vals(math.name) = doSomeMath(v1, v2, math.op)
-              found = true
+              foundVals = true
             case _ => ()
         case _ => ()
     }
   }
 
-  println("vals: " + vals.get("gvfh") + " " + vals.get("njlw"))
+  var foundHumn = false
+  var result = 0L
+  var currentMonkey = mathMonkeys.get("root").get
+  (vals.get(currentMonkey.leftMonkey), vals.get(currentMonkey.rightMonkey)) match
+    case (None, Some(v)) =>
+      vals(currentMonkey.leftMonkey) = v
+      result = v
+      if currentMonkey.leftMonkey == "humn" then
+        foundHumn = true
+      else
+        currentMonkey = mathMonkeys.get(currentMonkey.leftMonkey).get
+    case (Some(v), None) =>
+      vals(currentMonkey.rightMonkey) = v
+      result = v
+      if currentMonkey.rightMonkey == "humn" then
+        foundHumn = true
+      else
+        currentMonkey = mathMonkeys.get(currentMonkey.rightMonkey).get
+    case _ =>
+      throw new RuntimeException("Need fancier solution for monkey " + currentMonkey.name)
 
-  42
+
+  while (!foundHumn) {
+    (vals.get(currentMonkey.leftMonkey), vals.get(currentMonkey.rightMonkey)) match
+    case (None, Some(v)) =>
+      result = solveLeft(v, result, currentMonkey.op)
+      vals(currentMonkey.leftMonkey) = result
+      if currentMonkey.leftMonkey == "humn" then
+        foundHumn = true
+      else
+        currentMonkey = mathMonkeys.get(currentMonkey.leftMonkey).get
+    case (Some(v), None) =>
+      result = solveRight(v, result, currentMonkey.op)
+      vals(currentMonkey.rightMonkey) = result
+      if currentMonkey.rightMonkey == "humn" then
+        foundHumn = true
+      else
+        currentMonkey = mathMonkeys.get(currentMonkey.rightMonkey).get
+    case _ =>
+      throw new RuntimeException("Need fancier solution for monkey " + currentMonkey.name)
+  }
+
+  vals.get("humn").get
 
 @main def day21: Unit =
   val input = readInput()
