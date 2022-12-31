e5a5c4db6acc444888ff514ea00061918acf9798
gdenhez <gdenhez42@gmail.com>
2022-12-31 14:12:28 -0500

    Trying to understand lisp...

--- 2022/day22/day22.lisp
+++ 2022/day22/day22.lisp
@@ -1,17 +1,29 @@
-(defvar input (list))
-(let ((in (open "input_test.txt" :if-does-not-exist nil)))
-   (when in
-      (loop for line = (read-line in nil)
-      
-      while line do 
-        (setf input (cons line input)))
-      (close in)
-   )
-)
-(terpri)
-(write input)
+(defun read_input ()
+    (let ((in (open "input_test.txt" :if-does-not-exist nil)) (input (list)))
+        (when in
+            (loop for line = (read-line in nil)
+            
+            while line do 
+                (setf input (cons line input)))
+            (close in))
+        input
+    ))
+
+(defvar input (read_input))
+
 
 (defvar command (car input))
-(terpri)
 (write command)
+(terpri)
 
+(let ((input_map (reverse (cdr (cdr input)))) (width 0) (height 0))
+    (loop for line in input_map
+      do
+      (setf l (length line))
+      (if (> l width) (setf width l))
+      (setf height (+ 1 height)))
+    (write width)
+    (terpri)
+    (write height)
+    (terpri)
+)
