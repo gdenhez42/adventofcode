4ae6980efe9d693fc3fdfe031aa8b0a53ba97f8c
gdenhez <gdenhez42@gmail.com>
2022-12-31 13:12:19 -0500

    Read the input in lisp in a non functional way

--- 2022/day22/day22.lisp
+++ 2022/day22/day22.lisp
@@ -1,3 +1,17 @@
-(write-line "Hello World")
+(defvar input (list))
+(let ((in (open "input_test.txt" :if-does-not-exist nil)))
+   (when in
+      (loop for line = (read-line in nil)
+      
+      while line do 
+        (setf input (cons line input)))
+      (close in)
+   )
+)
+(terpri)
+(write input)
 
-(write-line "I am at 'Tutorials Point'! Learning LISP")
+(defvar command (car input))
+(terpri)
+(write command)
+
