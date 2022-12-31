09dc19dd3a881c4d0b2900ee1ea9e6f05a245045
gdenhez <gdenhez42@gmail.com>
2022-12-31 16:31:24 -0500

    lisp is wierd

--- 2022/day22/day22.lisp
+++ 2022/day22/day22.lisp
@@ -1,3 +1,9 @@
+(defstruct coord 
+   x
+   y
+)
+
+
 (defun read_input ()
     (let ((in (open "input_test.txt" :if-does-not-exist nil)) (input (list)))
         (when in
@@ -9,21 +15,45 @@
         input
     ))
 
+(defun build_board (input)
+    (let ((input_map (reverse (cdr (cdr input)))) (width 0) (height 0))
+        (loop for line in input_map
+            do
+            (setf l (length line))
+            (if (> l width) (setf width l))
+            (setf height (+ 1 height)))
+
+        (let ((i 0)  (v (make-array (list height width) :initial-element #\SPACE))  )
+            (loop for line in input_map
+                do
+                (loop for j from 0 to (- (length line) 1)
+                    do
+                        (setf (aref v i j) (aref line j))
+                )
+                (setf i (+ i 1))
+            )
+            v
+        )
+        
+    )
+)
+
+(defun find-first-non-space (board)
+    (let ((j 0))
+        (loop while (char= (aref board 0 j) #\space)
+            do (setf j (+ j 1))
+        )
+        (write (aref board 0 j))
+        (make-coord :x j :y 0)
+    )
+)
+
+
 (defvar input (read_input))
-
-
-(defvar command (car input))
-(write command)
+(defvar path (car input))
+(defvar board (build_board input))
+(defvar starting_pos (find-first-non-space board))
 (terpri)
 
-(let ((input_map (reverse (cdr (cdr input)))) (width 0) (height 0))
-    (loop for line in input_map
-      do
-      (setf l (length line))
-      (if (> l width) (setf width l))
-      (setf height (+ 1 height)))
-    (write width)
-    (terpri)
-    (write height)
-    (terpri)
-)
+
+(write board)
