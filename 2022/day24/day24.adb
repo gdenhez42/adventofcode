ab57bfd5a9b0358be32e15ea3ddba759b6f52d5f
gdenhez <gdenhez42@gmail.com>
2023-01-02 10:15:35 -0500

    I learned ada's records

--- 2022/day24/day24.adb
+++ 2022/day24/day24.adb
@@ -1,7 +1,21 @@
 with Ada.Text_IO;
 
 procedure day24 is
+
+    type Coord is record
+        X: Integer;
+        Y: Integer;
+    end record;
+
+    procedure Display_Coord (C: Coord) is
+    begin
+        Ada.Text_IO.Put_Line ("x:" & Integer'Image (C.X) & ", y:" & Integer'Image (C.Y));
+    end Display_Coord;
+
+    Current_Pos : Coord := (42,72);
+
 begin
-   --  Print "Hello, Ada!" to the screen
-   Ada.Text_IO.Put_Line ("Hello, Ada!");
+   --  A comment
+
+   Display_Coord (Current_Pos);
 end day24;
