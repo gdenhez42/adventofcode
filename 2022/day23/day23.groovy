98db7e41ca2d0ac7dd22794587c2847ddfb77980
gdenhez <gdenhez42@gmail.com>
2023-01-01 19:06:22 -0500

    Read file line by line

--- 2022/day23/day23.groovy
+++ 2022/day23/day23.groovy
@@ -1,2 +1,8 @@
 // execute with groovy day23.groovy
-print "Hello Groovy!\n"
+filename = 'input_test_tiny.txt'
+
+File fh = new File(filename)
+def lines = fh.readLines()
+for (line in lines) {
+    print line + "\n"
+}
