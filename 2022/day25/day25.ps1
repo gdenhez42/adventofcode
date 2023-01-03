bb49052861e6ab6588786bc14a0468149d09583b
Gabrielle Denhez <gdenhez42@gmail.com>
2023-01-03 07:58:03 -0500

    Day25 part 1... And there was no part 2 :'(... so done!

--- 2022/day25/day25.ps1
+++ 2022/day25/day25.ps1
@@ -19,7 +19,33 @@ function Snafu-To-Decimal {
     $result
 }
 
-foreach($line in Get-Content .\input_test.txt) {
-    $dec = Snafu-To-Decimal $line
-    write-host $dec
+function Decimal-To-Snafu {
+    param (
+        $decimal
+    )
+    $result = ""
+    while ($decimal -gt 0) {
+        $digit = $decimal % 5
+        $decimal = ($decimal - $digit) / 5
+        if ($digit -eq 4) {
+            $result = "-" + $result
+            $decimal += 1
+        } elseif ($digit -eq 3) {
+            $result = "=" + $result
+            $decimal += 1
+        } else {
+            $result = $digit.ToString() + $result
+        }
+    }
+    $result
 }
+
+
+$rez_decimal = 0
+foreach($line in Get-Content .\input.txt) {
+    $dec = Snafu-To-Decimal $line
+    $rez_decimal += $dec
+}
+
+$part1 = Decimal-To-Snafu $rez_decimal
+write-host $part1
