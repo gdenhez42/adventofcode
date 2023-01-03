b36ae6921a98929142a75964c3abc64f5bae5e45
Gabrielle Denhez <gdenhez42@gmail.com>
2023-01-02 20:04:07 -0500

    Snafu to decimal

--- 2022/day25/day25.ps1
+++ 2022/day25/day25.ps1
@@ -1,1 +1,25 @@
-Write-Host "Hello, damn Powershell!"
+
+function Snafu-To-Decimal {
+    param (
+        $snafu
+    )
+
+    $result = 0
+    foreach ($letter in $snafu.ToCharArray()) {
+        $result = $result * 5
+        Switch ($letter)
+        {
+            '0' { $result += 0 }
+            '1' { $result += 1 }
+            '2' { $result += 2 }
+            '-' { $result -= 1 }
+            '=' { $result -= 2 }
+        }
+    }
+    $result
+}
+
+foreach($line in Get-Content .\input_test.txt) {
+    $dec = Snafu-To-Decimal $line
+    write-host $dec
+}
