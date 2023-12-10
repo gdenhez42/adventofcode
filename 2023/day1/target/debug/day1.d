3e9e7526c8fe71274f2fe08acfd3d0d8791ae4ab
gdenhez <gdenhez42@gmail.com>
2023-12-10 16:47:21 -0500

    gitignore

--- 2023/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/gdenhez/git/adventofcode/2023/day1/target/debug/day1: /home/gdenhez/git/adventofcode/2023/day1/src/main.rs
--- 2023/day1/target/debug/deps/day1-1aef6317b1aa2a30.d
+++
@@ -1,5 +0,0 @@
-/home/gdenhez/git/adventofcode/2023/day1/target/debug/deps/day1-1aef6317b1aa2a30: src/main.rs
-
-/home/gdenhez/git/adventofcode/2023/day1/target/debug/deps/day1-1aef6317b1aa2a30.d: src/main.rs
-
-src/main.rs:
