b2a1207b07b0ead8e08d54c5961d2c9dffe8eaa4
Gabrielle Denhez <gdenhez42@gmail.com>
2025-12-01 18:11:05 -0500

    day1 skeleton and gitignore

---
+++ 2025/day1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var lines = File.ReadLines("input.txt").ToList();
+Console.WriteLine(lines[0]);
