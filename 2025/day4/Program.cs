c693392e13eb829c0c6ffd3627e38fe6679f724f
Gabrielle Denhez <gdenhez42@gmail.com>
2025-12-05 19:17:35 -0500

    days 4 and 5

---
+++ 2025/day4/Program.cs
@@ -0,0 +1,74 @@
+﻿var lines = File.ReadLines("input.txt").ToList();
+
+var grid = lines.Select(e1 => {
+    return e1.ToCharArray().Select(e2 => e2 == '@' ? 1 : 0).ToArray();
+}).ToArray();
+
+int count = 0;
+for (int i = 0; i < grid.Length; i++)
+{
+    for (int j = 0; j < grid[i].Length; j++)
+    {
+        if (grid[i][j] == 1)
+        {
+            int nbAdjacent = 0;
+            if (i > 0)
+            {
+                if (j > 0) nbAdjacent += grid[i-1][j-1];
+                nbAdjacent += grid[i-1][j];
+                if (j < grid[i].Length-1) nbAdjacent += grid[i-1][j+1];
+            }
+            if (i < grid[i].Length-1)
+            {
+                if (j > 0) nbAdjacent += grid[i+1][j-1];
+                nbAdjacent += grid[i+1][j];
+                if (j < grid[i].Length-1) nbAdjacent += grid[i+1][j+1];
+            }
+            if (j > 0) nbAdjacent += grid[i][j-1];
+            if (j < grid[i].Length-1) nbAdjacent += grid[i][j+1];
+
+            if (nbAdjacent < 4) count++;
+        }
+    }
+}
+Console.WriteLine($"Part1: {count}");
+
+bool canStillRemove = true;
+count = 0;
+while(canStillRemove)
+{
+    canStillRemove = false;
+    for (int i = 0; i < grid.Length; i++)
+    {
+        for (int j = 0; j < grid[i].Length; j++)
+        {
+            if (grid[i][j] == 1)
+            {
+                int nbAdjacent = 0;
+                if (i > 0)
+                {
+                    if (j > 0) nbAdjacent += grid[i-1][j-1];
+                    nbAdjacent += grid[i-1][j];
+                    if (j < grid[i].Length-1) nbAdjacent += grid[i-1][j+1];
+                }
+                if (i < grid[i].Length-1)
+                {
+                    if (j > 0) nbAdjacent += grid[i+1][j-1];
+                    nbAdjacent += grid[i+1][j];
+                    if (j < grid[i].Length-1) nbAdjacent += grid[i+1][j+1];
+                }
+                if (j > 0) nbAdjacent += grid[i][j-1];
+                if (j < grid[i].Length-1) nbAdjacent += grid[i][j+1];
+
+                if (nbAdjacent < 4)
+                {
+                    canStillRemove = true;
+                    grid[i][j] = 0;
+                    count++;
+                }
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Part2: {count}");
---
+++ 2025/day5/Program.cs
@@ -0,0 +1,62 @@
+﻿var lines = File.ReadLines("input.txt");
+
+var ranges = lines
+    .TakeWhile(e => e != "")
+    .Select(e => {
+        string[] r = e.Split('-');
+        return (long.Parse(r[0]), long.Parse(r[1]));
+    })
+    .ToList();
+
+var availables = lines
+    .SkipWhile(e => e != "")
+    .Skip(1)
+    .Select(e => {
+        return long.Parse(e);
+    })
+    .ToList();
+
+int count = 0;
+foreach (long a in availables)
+{
+    foreach ((long min, long max) in ranges)
+    {
+        if(a >= min && a <= max)
+        {
+            count++;
+            break;
+        }
+    }
+}
+Console.WriteLine($"Part1 : {count}");
+
+long nbFresh = 0;
+
+// Sort by size of range in descending order so the loop below works
+ranges.Sort(delegate((long,long) a, (long,long) b)
+{
+    (long a1,long a2) = a;
+    (long b1,long b2) = b;
+    return (b2-b1+1).CompareTo((a2-a1+1));
+});
+
+for (int i = 0; i < ranges.Count; i++)
+{
+    (long min, long max) = ranges[i];
+
+    for (int j = 0; j < i; j++)
+    {
+        (long rMin, long rMax) = ranges[j];
+
+        // If a previous range overlap the current one, shrink the current range
+        // The current range will not need to be split because any previous range is larger thanks to the sort above
+        if (min >= rMin && min <= rMax) min = rMax+1;
+        if (max >= rMin && max <= rMax) max = rMin-1;
+
+        if (min > max) break;
+    }
+
+    if (min <= max) nbFresh += max - min + 1;
+}
+
+Console.WriteLine($"Part2 : {nbFresh}");
